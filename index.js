a516334e093cb5cb1a98871b8d708523636b67ec
DAB804 <157375910+DAB804@users.noreply.github.com>
2024-11-09 23:55:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+
+btn1.onclick= ()=> document.body.style.Color= "gren";
+btn1.onclick= ()=> document.body.style.Color= "yellow";
+btn1.onclick= ()=> document.body.style.backgroundColor= "red";
+btn1.onclick= ()=> document.body.style.backgroundColor= "orange";
+btn1.onclick= ()=> document.body.style.backgroundColor= "grens";
